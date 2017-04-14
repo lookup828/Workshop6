465b8995e6ac57af8e7ed7ef7ce6ff744eea428d
lookup828 <tcloutier@umass.edu>
2017-04-14 14:05:30 -0400

    database
    database

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,6 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
-
-
 // You run the server from `server`, so `../client/build` is `server/../client/build`.
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
